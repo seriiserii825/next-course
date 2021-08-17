83c99444854e94e6de2407a50abd635310aae0a6
serii <seriiburduja@gmail.com>
2021-08-17 19:01:15 +0300

    single event

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useRouter } from "next/router";
+import { getEventById } from "../../dumy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const EventId = () => {
+  const router = useRouter();
+  const id = router.query.eventId;
+  const event = getEventById(id);
+
+  if (!id) {
+    return <p>Not found...</p>
+  }
   return (
     <div>
-      <h1>Event id</h1>
+      <EventSummary title={event.title}/>
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        <p className="description">{event.description}</p>
+      </EventContent>
     </div>
   );
 };
