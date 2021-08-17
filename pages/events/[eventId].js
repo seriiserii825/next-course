8a9b3ada9b53b86a1d9b84de7cfdcdbc1160573b
serii <seriiburduja@gmail.com>
2021-08-17 17:17:34 +0300

    create pages

---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventId = () => {
+  return (
+    <div>
+      <h1>Event id</h1>
+    </div>
+  );
+};
+
+export default EventId;
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllEvents = () => {
+  return (
+    <div>
+      <h1>All events</h1>
+    </div>
+  );
+};
+
+export default AllEvents;
